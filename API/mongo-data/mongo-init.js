23919554ab99bf9ee097c6750b6a0c13aded8ef5
Phwdm <ph.wildeman@student.avans.nl>
2024-02-29 11:26:00 +0100

    Added docker compose

---
+++ API/mongo-data/mongo-init.js
@@ -0,0 +1,18 @@
+// Create a new database and switch to it
+db = db.getSiblingDB('example');
+
+// Create a new collection and insert documents
+db.testcollection.insert([
+    { name: 'Document 1' },
+    { name: 'Document 2' },
+    { name: 'Document 3' }
+]);
+
+// Create a user with read and write privileges for the database
+db.createUser({
+    user: 'myuser',
+    pwd: 'mypassword',
+    roles: [
+        { role: 'readWrite', db: 'mydatabase' }
+    ]
+});
--- API/services/database.js
+++ API/services/database.js
@@ -5,6 +5,16 @@ const dbName = process.env.MONGO_DB || "mydb";
 const client = new MongoClient(uri);
 const db = client.db(dbName);
 
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://mongo:27017/mydatabase', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     auth: {
+//         user: 'root',
+//         password: 'example'
+//     }
+// });
+
 module.exports = {
     db: db,
     client: client
